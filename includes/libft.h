1de4955a9e87e28b9a917601564d900be774c3af
ohw1237@naver.com <ohw1237@naver.com>
2022-06-30 17:47:32 +0900

    ?

--- includes/libft.h
+++ includes/libft.h
@@ -59,6 +59,7 @@ size_t	ft_strlcat(char *dst, char *src, size_t size);
 size_t	ft_strlcpy(char *dst, char *src, size_t size);
 size_t	ft_strlen(const char *str);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+int		ft_strcmp(char *s1, char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strrchr(const char *s, int c);
